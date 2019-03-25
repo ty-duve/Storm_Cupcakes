2f3b97731cfb1ac099f15921ab8fc7bca166d35c
Ty Duve <ty.duve@my.simpson.edu>
2019-03-25 16:04:02 -0500

    Completed assignment 7.

--- public_html/index.php
+++ public_html/index.php
@@ -10,28 +10,8 @@
          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/journal/bootstrap.min.css">
      </head>
      <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.html">Storm Cupcakes</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a  class="nav-link" href="index.html">Home Page <span class="sr-only"></span></a>
-                </li>
-            
-                <li class="nav-item">
-                    <a class="nav-link" href="flavor.html">Flavors</a>
-                </li>
-            
-                <li class="nav-item">
-                    <a class="nav-link" href="location_hours.html">Hours and Location</a>
-                </li> 
-            </ul>
-            </div>
-        </nav>
+        <?php include '../resources/navigation.php'; ?>
+        
         <div class="container">
             <header>
                 <h1 style="text-align:center;font-size: 36px;
@@ -65,14 +45,12 @@
             <img src="confetticake.png" alt="Confetti Cupcake">
         </div>
             <img src="social_media.png" alt="Social Media" />
-        <footer>
-            <p>&copy; 2019 Storm Cupcake.<br />All rights reserved</p>
-        </footer>
+            <?php include '../resources/footer.php'; ?>
         </div>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+        
      </body>
 </html>
     
--- public_html/location_hours.php
+++ public_html/location_hours.php
@@ -10,28 +10,8 @@
          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.2.1/journal/bootstrap.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.html">Storm Cupcakes</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a  class="nav-link" href="index.html">Home Page</a>
-                </li>
-            
-                <li class="nav-item">
-                    <a class="nav-link" href="flavor.html">Flavors</a>
-                </li>
-            
-                <li class="nav-item active">
-                    <a class="nav-link" href="location_hours.html">Hours and Location <span class="sr-only"></span></a>
-                </li> 
-            </ul>
-            </div>
-        </nav>
+    <?php include '../resources/navigation.php'; ?>
+    
         <div class="container">
     <header>
         <h1>Contact <img src="cupcake.png" alt="Cupcake">
@@ -52,13 +32,12 @@
             <div class="col-sm-12 col-lg-4">
                 <img src="triocake.png" alt="Three Cupcakes">
             </div>
-<footer>
-    <p>&copy; 2019 Storm Cupcake.<br />All rights reserved</p>
-</footer>
+             <?php include '../resources/footer.php'; ?>
         </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
